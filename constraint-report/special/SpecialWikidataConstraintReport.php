4dacb98973e4848110d064954bdd2e5b9f26eb7e
jonaskeutel <jonas@keutel.de>
2015-01-26 14:30:11 +0100

    move functions outside the execute-function

--- constraint-report/special/SpecialWikidataConstraintReport.php
+++ constraint-report/special/SpecialWikidataConstraintReport.php
@@ -69,8 +69,31 @@ class SpecialWikidataConstraintReport extends SpecialPage {
 			}
 
 		}
+		
+		/*
+		$output = '';
 
-		function entityFromPar($parameter){
+		$dbr = wfGetDB( DB_SLAVE );
+		
+		$res = $dbr->select(
+			'page',									// $table
+			array( 'page_title' ),					// $vars (columns of the table)
+			'page_namespace = 122',					// $conds
+			__METHOD__,								// $fname = 'Database::select',
+			array( 'ORDER BY' => 'page_title ASC' )	// $options = array()
+		);
+		$output = '';
+		
+		foreach( $res as $row ) {
+			$output .= '[[Property:' . $row->page_title . "]]\n\n";
+		}
+ 
+		$wgOut->addWikiText( $output );
+		*/
+		
+	}
+	
+	function entityFromPar($parameter){
 			switch(strtoupper($par[0])) {
 			case 'Q':
 				return = $lookup->getEntity(new ItemId($par));
@@ -126,29 +149,5 @@ class SpecialWikidataConstraintReport extends SpecialPage {
 		function checkSymmetricConstraint( $propertyId ,$dataValue ) {
 			//todo
 		}
-
-
-		
-		/*
-		$output = '';
-
-		$dbr = wfGetDB( DB_SLAVE );
-		
-		$res = $dbr->select(
-			'page',									// $table
-			array( 'page_title' ),					// $vars (columns of the table)
-			'page_namespace = 122',					// $conds
-			__METHOD__,								// $fname = 'Database::select',
-			array( 'ORDER BY' => 'page_title ASC' )	// $options = array()
-		);
-		$output = '';
-		
-		foreach( $res as $row ) {
-			$output .= '[[Property:' . $row->page_title . "]]\n\n";
-		}
- 
-		$wgOut->addWikiText( $output );
-		*/
-		
-	}
+	
 }
