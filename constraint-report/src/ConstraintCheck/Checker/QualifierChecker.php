b06695a12c0933b4f03f878b7f5a0da2bb34142d
Andreas Burmeister <andreas.burmeister@student.hpi.de>
2015-02-20 17:32:07 +0100

    unified format of parameter column in constraint report output table

--- constraint-report/src/ConstraintCheck/Checker/QualifierChecker.php
+++ constraint-report/src/ConstraintCheck/Checker/QualifierChecker.php
@@ -23,6 +23,9 @@ class QualifierChecker
 
     public function checkQualifiersConstraint($propertyId, $dataValueString, $statement, $list)
     {
+        $toReplace = array("{", "}", "|", " ");
+        $parameterString = $this->helper->limitOutput( 'list: ' . str_replace($toReplace, '', $list) );
+
         $list = $this->helper->toArray( $list );
         $status = 'compliance';
 
@@ -33,7 +36,7 @@ class QualifierChecker
                 break;
             }
         }
-        return new CheckResult($propertyId, $dataValueString, "Qualifiers", '\'\'(none)\'\'', $status );
+        return new CheckResult($propertyId, $dataValueString, "Qualifiers", $parameterString, $status );
     }
 
 }
