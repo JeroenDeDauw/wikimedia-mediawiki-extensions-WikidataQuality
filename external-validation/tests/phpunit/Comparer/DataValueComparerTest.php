03f072a2251059bb1f62b787e223a1550a124d50
Sören Oldag <soeren_oldag@freenet.de>
2015-03-02 10:33:43 +0100

    Enabled DataValueTest for GlobeCoordinateValueComparer.

--- external-validation/tests/phpunit/Comparer/DataValueComparerTest.php
+++ external-validation/tests/phpunit/Comparer/DataValueComparerTest.php
@@ -63,8 +63,8 @@ class DataValueComparerTest  extends \PHPUnit_Framework_TestCase {
             array( new MultilingualTextValue( array( new MonolingualTextValue( 'en', 'foo' ) ) ), 'WikidataQuality\ExternalValidation\CrossCheck\Comparer\MultilingualTextValueComparer' ),
             array( new QuantityValue( new  DecimalValue( 42 ), '1', new DecimalValue( 42 ), new DecimalValue(42 ) ), 'WikidataQuality\ExternalValidation\CrossCheck\Comparer\QuantityValueComparer' ),
             array( new StringValue( 'foo' ), 'WikidataQuality\ExternalValidation\CrossCheck\Comparer\StringValueComparer' ),
-            array( new TimeValue( '+00000002013-12-07T00:00:00Z', 0, 0, 0, 11, 'http://www.wikidata.org/entity/Q1985727' ), 'WikidataQuality\ExternalValidation\CrossCheck\Comparer\TimeValueComparer' )/*,
-            array(new GlobeCoordinateValue( new LatLongValue(52.5, 13.3), 0.016), 'WikidataQuality\ExternalValidation\CrossCheck\Comparer\GlobeCoordinateValueComparer')*/
+            array( new TimeValue( '+00000002013-12-07T00:00:00Z', 0, 0, 0, 11, 'http://www.wikidata.org/entity/Q1985727' ), 'WikidataQuality\ExternalValidation\CrossCheck\Comparer\TimeValueComparer' ),
+            array( new GlobeCoordinateValue( new LatLongValue( 52.5, 13.3 ), 0.016 ), 'WikidataQuality\ExternalValidation\CrossCheck\Comparer\GlobeCoordinateValueComparer' )
         );
     }
 
