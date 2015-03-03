65892ec9a117b77a67ffa99852067d6750df9824
Sören Oldag <soeren_oldag@freenet.de>
2015-03-03 14:05:41 +0100

    Refactored XPathEvaluatorTest and added MappingEvaluatorTest.

--- external-validation/tests/phpunit/MappingEvaluator/XPathEvaluatorTest.php
+++ external-validation/tests/phpunit/MappingEvaluator/XPathEvaluatorTest.php
@@ -2,8 +2,9 @@
 
 namespace WikidataQuality\ExternalValidation\Test\MappingEvaluator;
 
-use WikidataQuality\ExternalValidation\CrossCheck\MappingEvaluator\MappingEvaluator;
+
 use WikidataQuality\ExternalValidation\CrossCheck\MappingEvaluator\XPathEvaluator;
+
 
 /**
  * @covers WikidataQuality\ExternalValidation\CrossCheck\MappingEvaluator\XPathEvaluator
@@ -18,49 +19,73 @@ use WikidataQuality\ExternalValidation\CrossCheck\MappingEvaluator\XPathEvaluato
  */
 class XPathEvaluatorTest extends \PHPUnit_Framework_TestCase
 {
-    private $testData;
-    private $mapping;
-    private $evaluator;
-
-
-    protected function setUp()
+    /**
+     * @dataProvider constructDataProvider
+     */
+    public function testConstruct( $externalData, $expectedException )
     {
-        parent::setUp();
-        $this->testData = file_get_contents( __DIR__ . '/XMLData.xml' );
-        require __DIR__ . '/mapping.inc.php';
-        $this->mapping = $mapping;
-        $this->evaluator = new XPathEvaluator( $this->testData );
+        $this->setExpectedException( $expectedException );
+
+        $evaluator = new XPathEvaluator( $externalData );
+
+        $this->assertEquals( $externalData, $evaluator->getExternalData() );
     }
 
-    protected function tearDown()
+    /**
+     * Test cases for testConstruct
+     * @return array
+     */
+    public function constructDataProvider()
     {
-        unset( $this->evaluator, $this->mapping, $this->testData );
-        parent::tearDown();
+        return array(
+            array(
+                '<foobar />',
+                null
+            ),
+            array(
+                'foobar',
+                'InvalidArgumentException'
+            )
+        );
     }
 
 
     /**
-     * @covers WikidataQuality\ExternalValidation\CrossCheck\MappingEvaluator\XPathEvaluator::evaluate
+     * @dataProvider evaluateDataProvider
      */
-    public function testEvaluate()
+    public function testEvaluate( $externalData, $nodeSelector, $valueFormatter, $expectedResult )
     {
-        $nodeSelector = $this->mapping[ 'testcase one' ][ 'nodeSelector' ];
-        $this->assertEquals( array( 'success' ), $this->evaluator->evaluate( $nodeSelector ), 'should find path' );
-
-        $nodeSelector = $this->mapping[ 'testcase two' ][ 'nodeSelector' ];
-        $valueFormatter = $this->mapping[ 'testcase two' ][ 'valueFormatter' ];
-        $this->assertEquals( array( 'success' ), $this->evaluator->evaluate( $nodeSelector, $valueFormatter ), 'should format string' );
-
-        $nodeSelector = $this->mapping[ 'testcase three' ][ 'nodeSelector' ];
-        $this->assertEquals( array(), $this->evaluator->evaluate( $nodeSelector, $valueFormatter ), 'should find nothing' );
+        $evaluator = new XPathEvaluator( $externalData );
+        $this->assertEquals( $expectedResult, $evaluator->evaluate( $nodeSelector, $valueFormatter ) );
     }
 
     /**
-     * @covers WikidataQuality\ExternalValidation\CrossCheck\MappingEvaluator\XPathEvaluator::getEvaluator
+     * Test cases for testEvaluate
+     * @return array
      */
-    public function testGetEvaluator()
+    public function evaluateDataProvider()
     {
-        $evaluator = MappingEvaluator::getEvaluator( 'xml', $this->testData );
-        $this->assertEquals( 'WikidataQuality\ExternalValidation\CrossCheck\MappingEvaluator\XPathEvaluator', get_class( $evaluator ), 'should get XPathEvaluator' );
+        $testData = file_get_contents( __DIR__ . '/testdata/data.xml' );
+
+        return array(
+            array(
+                $testData,
+                '/test/testcase[@case="one" and @result="true"]',
+                null,
+                array( 'success' )
+            ),
+            array(
+                $testData,
+                '/test/testcase[@case="two"]/result',
+                'concat(substring-after(./text(), "."), substring-before(./text(), "."))',
+                array( 'success' )
+            ),
+            array(
+                $testData,
+                '/test/testcase[@case="tree"]/result',
+                null,
+                array()
+            )
+        );
     }
 }
