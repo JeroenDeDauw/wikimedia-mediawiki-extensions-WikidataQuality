6d298858dd71d6e8de491ba5a506156cc1773ac9
Sören Oldag <soeren_oldag@freenet.de>
2015-03-12 16:38:53 +0100

    SQL bugfix.

--- external-validation/sql/create_wdq_dump_information.sql
+++ external-validation/sql/create_wdq_dump_information.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS wdq_dump_information (
   row_id          BIGINT UNSIGNED  NOT NULL PRIMARY KEY AUTO_INCREMENT,
   source_item_id  INT(10)          NOT NULL,
-  import_date     TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  import_date     TIMESTAMP        NOT NULL,
   format          VARBINARY(30)    NOT NULL,
   language        VARBINARY(10)    NOT NULL,
   source_url      VARBINARY(300)   NOT NULL,
