addae4d1d4ade6ecc9ee9e7727ee0be388bc621d
Andreas Burmeister <andreas.burmeister@student.hpi.de>
2015-03-31 15:24:36 +0200

    minor sql syntax fix

--- constraint-report/sql/create_wdq_constraints.sql
+++ constraint-report/sql/create_wdq_constraints.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE IF NOT EXISTS `wdq_constraints` (
+﻿CREATE TABLE IF NOT EXISTS wdq_constraints (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `pid` int(11) DEFAULT NULL,
   `constraint_name` varchar(255) DEFAULT NULL,
